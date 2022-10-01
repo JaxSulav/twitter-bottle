629d356e6df38ab8af6facdd964a664825ac6735
Sulav Shrestha <gilm.dav007@gmail.com>
2022-10-01 09:59:21 +0545

    - Initial Commit
    - Add sqlite integration
    - Add user and session table
    - Save user in signup
    - Handle registering duplicate user

---
+++ models/user.py
@@ -0,0 +1,54 @@
+
+
+from logging import exception
+import sqlite3
+
+def create_user_model(dbConn: sqlite3.Connection):
+    try:
+        dbConn.execute("CREATE TABLE user (id INTEGER PRIMARY KEY, user_id char(50) NOT NULL, first_name char(200) NOT NULL, last_name char(200) NOT NULL, email char(200) NOT NULL, username char(200) NOT NULL UNIQUE, password char(200) NOT NULL)")
+        print("User table Created")
+    except sqlite3.OperationalError:
+        print("User table already exists")
+    except Exception as e:
+        print(f"Error: {e}")
+
+def create_session_model(dbConn: sqlite3.Connection):
+    try:
+        dbConn.execute("CREATE TABLE session (id INTEGER PRIMARY KEY, session_id char(50) NOT NULL UNIQUE)")
+        print("Session table Created")
+    except sqlite3.OperationalError:
+        print("Session table already exists")
+    except Exception as e:
+        print(f"Error: {e}")
+
+def insert_user(dbConn: sqlite3.Connection, **data):
+    user_id = data.get("user_id", None)
+    first_name = data.get("user_first_name", None)
+    last_name = data.get("user_last_name", None)
+    email = data.get("user_email", None)
+    username = data.get("user_name", None)
+    password = data.get("user_password", None)
+
+    sql_query = "INSERT INTO user (user_id, first_name, last_name, email, username, password) VALUES (?,?,?,?,?,?)"
+    args = user_id, first_name, last_name, email, username, password
+    try:
+        dbConn.execute(sql_query, args)
+    except sqlite3.IntegrityError:
+        return False, "Username already exists"
+    except Exception as e:
+        print(f"Error: {e}")
+        return False, "Internal Error, contact admin"
+    print(f"Inserted {args} into user table")
+    return True, f"User {username} created"
+
+def insert_session(dbConn: sqlite3.Connection, session_id):
+    sql_query = "INSERT INTO session (session_id) VALUES (?)"
+    try:
+        dbConn.execute(sql_query, session_id)
+    except sqlite3.IntegrityError:
+        return False, "Session id already exists"
+    except Exception as e:
+        print(f"Error: {e}")
+        return False, "Internal Error, contact admin"
+    print(f"Inserted {session_id} into session table")
+    return True, f"Session {session_id} created"
